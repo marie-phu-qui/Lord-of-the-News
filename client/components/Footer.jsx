44a66e48a407fbce08de8b160cf8df5835141c58
harrison@devacademy.co.nz <harrison-symes>
2018-09-27 17:34:04 +1200

    footer is here loving the footer

---
+++ client/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = props => {
+  return (
+    <div id='footer'>
+      <a href='https://github.com/marie-phu-qui/Lord-of-the-News/blob/master/README.md'>
+        Our github
+      </a>
+      <p>
+        The news API is provided by Google News. Thank you Google News
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- public/bundle.js
+++ public/bundle.js
@@ -200,6 +200,10 @@ var _ArticleList = __webpack_require__(/*! ./ArticleList */ "./client/components
 
 var _ArticleList2 = _interopRequireDefault(_ArticleList);
 
+var _Footer = __webpack_require__(/*! ./Footer */ "./client/components/Footer.jsx");
+
+var _Footer2 = _interopRequireDefault(_Footer);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -251,12 +255,8 @@ var App = function (_React$Component) {
         _react2.default.Fragment,
         null,
         _react2.default.createElement(_Header2.default, null),
-        _react2.default.createElement(
-          'button',
-          { onClick: this.transGollum },
-          'GOLLUM'
-        ),
-        _react2.default.createElement(_ArticleList2.default, null)
+        _react2.default.createElement(_ArticleList2.default, null),
+        _react2.default.createElement(_Footer2.default, null)
       );
     }
   }]);
@@ -417,6 +417,47 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch) {
 };
 
 exports.default = (0, _reactRedux.connect)(mapStateToProps, mapDispatchToProps)(ArticleList);
+
+/***/ }),
+
+/***/ "./client/components/Footer.jsx":
+/*!**************************************!*\
+  !*** ./client/components/Footer.jsx ***!
+  \**************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Footer = function Footer(props) {
+  return _react2.default.createElement(
+    'div',
+    { id: 'footer' },
+    _react2.default.createElement(
+      'a',
+      { href: 'https://github.com/marie-phu-qui/Lord-of-the-News/blob/master/README.md' },
+      'Our github'
+    ),
+    _react2.default.createElement(
+      'p',
+      null,
+      'The news API is provided by Google News. Thank you Google News'
+    )
+  );
+};
+
+exports.default = Footer;
 
 /***/ }),
 
