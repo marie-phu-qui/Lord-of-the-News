f467610ae27f7492577da84a4fe0f4d93fae038f
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-09-29 11:58:19 +1200

    WE ARE PREDICTING - without logic

--- server/tensorflow/tf_layers/sketch.js
+++ server/tensorflow/tf_layers/sketch.js
@@ -1,7 +1,7 @@
 // BUILD OUR SEQUENTIAL MODEL
 const model = tf.sequential()
 
-
+// Config first layer = inputShape mandatory 
 const configHid = {
   units: 4,
   inputShape: [2],
@@ -9,21 +9,32 @@ const configHid = {
 }
 
 const hidden = tf.layers.dense(configHid)
+model.add(hidden)
 
+// Config Second layer  
 const configOut = {
   units: 3,
   activation: 'sigmoid'
 }
 
 const output = tf.layers.dense(configOut)
-
-model.add(hidden)
 model.add(output)
 
+// Optimizer with gradient descent
 const sgdOpt = tf.train.sgd(0.1)
+
+// Compile the code
 const config = {
   optimizer: sgdOpt,
-  loss: 'Boooh'
+  loss: 'meanSquaredError'
 }
+model.compile(config)
 
-model.compile()
+// We need inputs from a tensor
+const inputs = tf.tensor2d([
+  [0.25, 0.92]
+])
+
+
+let outputs = model.predict(inputs)
+outputs.print()
