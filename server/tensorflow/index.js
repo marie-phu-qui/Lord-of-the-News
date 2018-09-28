bd40a891c3b5425f10dfec9c981765b8dc66be5b
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-09-28 19:57:59 +1200

    add tensorflow folder in server to train

--- server/tensorflow/index.js
+++ server/tensorflow/index.js
@@ -1,48 +1,47 @@
-/**
-* @license
-* Copyright 2018 Google LLC. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http:// www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* ==============================================================================
-*/
-
-// This tiny example illustrates how little code is necessary build /
-// train / predict from a model in TensorFlow.js.  Edit this code
-// and refresh the index.html to quickly explore the API.
-
 // Tiny TFJS train / predict example.
-async function myFirstTfjs() {
-  // Create a simple model.
-  const model = tf.sequential();
-  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+// async function myFirstTfjs() {
+//   // Create a simple model.
+//   const model = tf.sequential();
+//   model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 
-  // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({
-    loss: 'meanSquaredError',
-    optimizer: 'sgd'
-  });
+//   // Prepare the model for training: Specify the loss and the optimizer.
+//   model.compile({
+//     loss: 'meanSquaredError',
+//     optimizer: 'sgd'
+//   });
 
-  // Generate some synthetic data for training. (y = 2x - 1)
-  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-  const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+//   // Generate some synthetic data for training. (y = 2x - 1)
+//   const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+//   const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
 
-  // Train the model using the data.
-  await model.fit(xs, ys, { epochs: 250 });
+//   // Train the model using the data.
+//   await model.fit(xs, ys, { epochs: 250 });
 
-  // Use the model to do inference on a data point the model hasn't seen.
-  // Should print approximately 39.
-  document.getElementById('micro_out_div').innerText +=
-    model.predict(tf.tensor2d([20], [1, 1]));
+//   // Use the model to do inference on a data point the model hasn't seen.
+//   // Should print approximately 39.
+//   document.getElementById('micro_out_div').innerText +=
+//     model.predict(tf.tensor2d([20], [1, 1]).print);
+// }
+
+// myFirstTfjs();
+
+
+// should print [1,2,3,4] on our console if we write this code there
+function draw() {
+  const data = tf.tensor([1, 2, 3, 4])
+  const values = [];
+  for (let i = 0; i < 15; i++) {
+    values[i] = Math.random(0, 100);
+  }
+  const shape = [5, 3];
+  const a = tf.tensor2d(values, shape, 'int32')
+  const b = tf.tensor2d(values, shape, 'int32')
+
+  const bb = b.transpose()
+
+  const c = a.matMul(bb)
+  c.print()
 }
 
-myFirstTfjs();
+draw()
+console.log('hello');
