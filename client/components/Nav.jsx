3ac6109f4d5b8a8bef262db7d65a6782fd372b79
Nat Sirisamphan <nat_sirisamphan@hotmail.com>
2018-09-28 12:19:07 +1200

    css

---
+++ client/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+const Nav = () => {
+    return (
+       
+<nav class="navbar navbar-expand-lg ">
+  
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      
+      <li class="nav-item">
+        <a class="nav-link"  href="#">Mean</a>
+      
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kind</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lordifty</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+)
+}
+
+export default Nav
