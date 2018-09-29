a427364651d67012cc523949164aedae57d30a38
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-09-29 15:53:30 +1200

    switched class to className per React

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -10,41 +10,76 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 function topFunction() {
-    document.documentElement.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
 }
 
-const Header = () => {
-  return (  
-      <div id='Header'> 
-    <h1>Lord of the News</h1>
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // showAbout: false,
+      // showTeam: false,
+      // showContact: false,
+    }
+    // this.toggleMore = this.toggleMore.bind(this)
+    // this.toggleTeam = this.toggleTeam.bind(this)
+    this.showTeam = this.showTeam.bind(this)
+  }
 
-<div id="myNav" class="overlay">
-  <a href="javascript:void(0)" class="closebtn" onClick={()=> closeNav()}>&times;</a>
-  <div class="overlay-content">
-    <a href="#">About</a>
-    <a href="#">Meet the Team</a>
-    <a href="#">Contact</a>
-  </div>
-</div>
-<div id='navpopup'>
-<button onClick={()=> openNav()}> More  </button>
-<div id='myBtn'>
- <button onClick={()=>topFunction()}> Top </button>
-</div>
-</div>
+  // toggleMore() {
+  //   this.setState({
+  //     showAbout: false,
+  //     showTeam: false,
+  //     showContact: false,
+  //   });
+  // }
 
-    </div>
-  )
+  // toggleTeam() {
+  //   this.setState({
+  //     showAbout: false,
+  //     showTeam: true,
+  //     showContact: false,
+  //   });
+  // }
+
+  showTeam() {
+    return (
+      <Meet />
+    )
+  }
+
+  render() {
+    return (
+      <div id='Header'>
+        <h1>Lord of the News</h1>
+
+        <div id="myNav" className="overlay">
+          <a href="javascript:void(0)" className="closebtn" onClick={() => closeNav()}>&times;</a>
+          <div className="overlay-content">
+            <a href="#">About</a>
+            <a onClick={() => this.showTeam()}> Meet the Team</a>
+            <a href="#">Contact</a>
+          </div>
+        </div>
+        <div id='navpopup'>
+          <button onClick={() => openNav()}> More  </button>
+          <div id='myBtn'>
+            <button onClick={() => topFunction()}> Top </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 export default Header
 
--- public/bundle.js
+++ public/bundle.js
@@ -510,6 +510,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 
 var _react2 = _interopRequireDefault(_react);
@@ -519,6 +521,12 @@ var _Meet = __webpack_require__(/*! ./Meet */ "./client/components/Meet.jsx");
 var _Meet2 = _interopRequireDefault(_Meet);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
@@ -543,69 +551,120 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-var Header = function Header() {
-  return _react2.default.createElement(
-    "div",
-    { id: "Header" },
-    _react2.default.createElement(
-      "h1",
-      null,
-      "Lord of the News"
-    ),
-    _react2.default.createElement(
-      "div",
-      { id: "myNav", "class": "overlay" },
-      _react2.default.createElement(
-        "a",
-        { href: "javascript:void(0)", "class": "closebtn", onClick: function onClick() {
-            return closeNav();
-          } },
-        "\xD7"
-      ),
-      _react2.default.createElement(
+var Header = function (_React$Component) {
+  _inherits(Header, _React$Component);
+
+  function Header(props) {
+    _classCallCheck(this, Header);
+
+    var _this = _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).call(this, props));
+
+    _this.state = {}
+    // showAbout: false,
+    // showTeam: false,
+    // showContact: false,
+
+    // this.toggleMore = this.toggleMore.bind(this)
+    // this.toggleTeam = this.toggleTeam.bind(this)
+    ;_this.showTeam = _this.showTeam.bind(_this);
+    return _this;
+  }
+
+  // toggleMore() {
+  //   this.setState({
+  //     showAbout: false,
+  //     showTeam: false,
+  //     showContact: false,
+  //   });
+  // }
+
+  // toggleTeam() {
+  //   this.setState({
+  //     showAbout: false,
+  //     showTeam: true,
+  //     showContact: false,
+  //   });
+  // }
+
+  _createClass(Header, [{
+    key: "showTeam",
+    value: function showTeam() {
+      return _react2.default.createElement(_Meet2.default, null);
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      return _react2.default.createElement(
         "div",
-        { "class": "overlay-content" },
+        { id: "Header" },
         _react2.default.createElement(
-          "a",
-          { href: "#" },
-          "About"
+          "h1",
+          null,
+          "Lord of the News"
         ),
         _react2.default.createElement(
-          "a",
-          { href: "#" },
-          "Meet the Team"
+          "div",
+          { id: "myNav", className: "overlay" },
+          _react2.default.createElement(
+            "a",
+            { href: "javascript:void(0)", className: "closebtn", onClick: function onClick() {
+                return closeNav();
+              } },
+            "\xD7"
+          ),
+          _react2.default.createElement(
+            "div",
+            { className: "overlay-content" },
+            _react2.default.createElement(
+              "a",
+              { href: "#" },
+              "About"
+            ),
+            _react2.default.createElement(
+              "a",
+              { onClick: function onClick() {
+                  return _this2.showTeam();
+                } },
+              " Meet the Team"
+            ),
+            _react2.default.createElement(
+              "a",
+              { href: "#" },
+              "Contact"
+            )
+          )
         ),
         _react2.default.createElement(
-          "a",
-          { href: "#" },
-          "Contact"
+          "div",
+          { id: "navpopup" },
+          _react2.default.createElement(
+            "button",
+            { onClick: function onClick() {
+                return openNav();
+              } },
+            " More  "
+          ),
+          _react2.default.createElement(
+            "div",
+            { id: "myBtn" },
+            _react2.default.createElement(
+              "button",
+              { onClick: function onClick() {
+                  return topFunction();
+                } },
+              " Top "
+            )
+          )
         )
-      )
-    ),
-    _react2.default.createElement(
-      "div",
-      { id: "navpopup" },
-      _react2.default.createElement(
-        "button",
-        { onClick: function onClick() {
-            return openNav();
-          } },
-        " More  "
-      ),
-      _react2.default.createElement(
-        "div",
-        { id: "myBtn" },
-        _react2.default.createElement(
-          "button",
-          { onClick: function onClick() {
-              return topFunction();
-            } },
-          " Top "
-        )
-      )
-    )
-  );
-};
+      );
+    }
+  }]);
+
+  return Header;
+}(_react2.default.Component);
+
 exports.default = Header;
 
 /***/ }),
@@ -665,37 +724,37 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var Nav = function Nav() {
   return _react2.default.createElement(
     "nav",
-    { "class": "navbar navbar-expand-lg " },
+    { className: "navbar navbar-expand-lg " },
     _react2.default.createElement(
       "div",
-      { "class": "collapse navbar-collapse", id: "navbarNav" },
+      { className: "collapse navbar-collapse", id: "navbarNav" },
       _react2.default.createElement(
         "ul",
-        { "class": "navbar-nav" },
+        { className: "navbar-nav" },
         _react2.default.createElement(
           "li",
-          { "class": "nav-item" },
+          { className: "nav-item" },
           _react2.default.createElement(
             "a",
-            { "class": "nav-link", href: "#" },
+            { className: "nav-link", href: "#" },
             "English"
           )
         ),
         _react2.default.createElement(
           "li",
-          { "class": "nav-item" },
+          { className: "nav-item" },
           _react2.default.createElement(
             "a",
-            { "class": "nav-link", href: "#" },
+            { className: "nav-link", href: "#" },
             "Gollumify"
           )
         ),
         _react2.default.createElement(
           "li",
-          { "class": "nav-item" },
+          { className: "nav-item" },
           _react2.default.createElement(
             "a",
-            { "class": "nav-link", href: "#" },
+            { className: "nav-link", href: "#" },
             "Nazgul"
           )
         )
