47e7bfd186b2977949c0937c4c3ab5284d682e6c
Kyoko Pohe <Kyokz@Kyokos-MacBook-Pro.local>
2018-10-01 21:06:49 +1300

    reducers tests passing

--- tests/reducer.test.js
+++ tests/reducer.test.js
@@ -1,81 +1,39 @@
-import reducer, {news, language} from '../client/reducers/index'
-// import * as types from '../client/actions/index'
+// Reducer to be tested
+import selectReducer from '../client/reducers/index.js'
+import {
+    RECEIVE_NEWS, CHANGE_LANGUAGE // Only ones related to the reducer being tested
+} from '../client/actions/index.js';
 
-// test('should return the initial news state', () => {
-//   //Arrange
-//   const expected = {
-//       news: "",
-//       language: null
-//   }
-//   //Act
-//   const actual = reducer(undefined, {})
-//   //Assert
-//   expect(actual).toEqual(expected)
-// })
-
-
-// test('should handle RECEIVE_NEWS', () => {
-//   //Arrange
-//   const fakeNews = "something interesting"
-//   const action = {
-//     type: 'RECEIVE_NEWS',
-//     news: fakeNews
-//   }
-//   const expected = [fakeNews]
-
-//   //Act
-//   const actual = reducer([], action)
-//   //Assert
-//   expect(actual).toEqual(expected)
-
-// })
-
-
-// test('should handle CHANGE_LANGUAGE', () => {
-//   //Arrange
-//   const fakeLanguage = "gollum"
-//   const action = {
-//     type: 'CHANGE_LANGUAGE',
-//     language: fakeLanguage
-//   }
-
-//   const expected = [...language, fakeLanguage]
-
-//   //Act
-//   const actual = reducer(undefined, action)
-//   //Assert
-//   expect(actual).toEqual(expected)
-
-// })
-
-describe('reducer', () => {
-    const initialState = reducer(undefined, {})
-
-    it('receives news', () => {
-        const news = [{}, {}]
-
-        const result = reducer(initialState, actions.news(news))
-
-        expect(result).toEqual({
-            ...intialState,
-            news: RemoteData.success(news),        
-        })
-    })
-
-    it('changes the language', () => {
-        const language = [{language: ""}]
-        const state = reducer(initialState, actions.language(language))
-        
-        const updatedLanguage = {language: translate}
-
-        const result = reducer(state, {
-            type: actions.CHANGE_LANGUAGE,
-            payload: updatedLanguage,
-        })
-
-        expect(result).toEqual({
-            ...state,
-            language: RemoteData.success(updatedLanguage)
-        })
-    })
+test('should return the initial news state', () => {
+  //Arrange
+  const expected = {
+      news: "",
+      language: null
+  }
+  //Act
+  const actual = selectReducer(undefined, {})
+  //Assert
+  expect(actual).toEqual(expected)
 })
+
+
+describe('select_reducer', () => {
+
+    describe('RECEIVE_NEWS', () => {
+        test('returns the correct state', () => {
+          const action = { type: RECEIVE_NEWS };
+          const expectedState = { news: "", language: null };
+      
+          expect(selectReducer(undefined, action)).toEqual(expectedState);
+        });
+      });
+
+    describe('CHANGE_LANGUAGE', () => {
+        test('returns the correct state', () => {
+            const action = { type: CHANGE_LANGUAGE };
+            const expectedState = { news: "", language: null };
+        
+            expect(selectReducer(undefined, action)).toEqual(expectedState);
+        });
+    });
+});
