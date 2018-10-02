e145413a7b98f876bd70bbe5c7209d11f677bfb6
Kyoko Pohe <Kyokz@Kyokos-MacBook-Pro.local>
2018-10-03 10:39:52 +1300

    nazgul and gollum tests

--- tests/client/TranslateButtons.test.js
+++ tests/client/TranslateButtons.test.js
@@ -9,12 +9,19 @@ test('<TranslateButtons /> renders without crashing', () => {
     shallow(<TranslateButtons />);
   });
 
+  //Calling for state to change to Gollumify/ Nazgulify
   describe('<TranslateButtons />', () => {
     describe('onClick()', () => {
-      test('successfully calls the changeLanguage handler', () => {
+      test('successfully calls the changeLanguage handler for gollum', () => {
         const mockChange = jest.fn();
         const wrapper = shallow(<TranslateButtons changeLanguage={mockChange}/>);
-        wrapper.find('button').at(0).simulate('click');
+        wrapper.find('button').at(1).simulate('click');
+        expect(mockChange.mock.calls.length).toEqual(1);
+      });
+      test('successfully calls the changeLanguage handler for Nazgul', () => {
+        const mockChange = jest.fn();
+        const wrapper = shallow(<TranslateButtons changeLanguage={mockChange}/>);
+        wrapper.find('button').at(2).simulate('click');
         expect(mockChange.mock.calls.length).toEqual(1);
       });
     });
