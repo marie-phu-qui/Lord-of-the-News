318e3d52ee3c3ceaba4272c0219be5442b45f887
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-11-02 21:18:04 +1300

    new tests

---
+++ tests/client/About.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import '../setup-env.js'; // Setup Enzyme & Adapter
+
+import {About} from '../../client/components/About'
+
+
+describe('<About />', () => {
+
+    test('renders without crashing', () => {
+        shallow(<About />);
+    });
+})
